eea1e952d6d238ad2c4bcb372608796abab99f87
Felipe <felipe.m.c.b.fm@gmail.com>
2024-06-23 21:53:58 -0300

    criação do servidor e definição de porta

---
+++ index.js
@@ -0,0 +1,18 @@
+import chalk from "chalk"
+import express, { json } from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+
+
+dotenv.config()
+
+//definição de variáveis
+const PORT = process.env.PORT
+
+//Configuração do servidor
+const app = express()
+app.use(cors())
+app.use(json())
+
+
+app.listen(PORT, ()=>console.log(chalk.green("servidor funcionando")))
