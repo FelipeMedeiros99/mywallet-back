46b3b13ccdf1cbd5a8d2702ee15ffb32a7333777
Felipe <felipe.m.c.b.fm@gmail.com>
2024-06-24 01:47:23 -0300

    senha do usuário criptografada

--- controllers/novo-usuario-controller.js
+++ controllers/novo-usuario-controller.js
@@ -1,11 +1,21 @@
 import chalk from "chalk"
+import bcrypt from "bcrypt"
 
 import { novoUsuarioSchema } from "../schemas.js"
+import db from "../banco.js"
 
+
+// TODO: adicionar validação para ver se o usuário ou email já existe
+// TODO: CRIPTOGRAFAR SENHA AO SALVAR NO SERVIDOR
 export async function novoUsuarioPost(req, res){
     const {body:dados} = req
     try{
         const validacao = await novoUsuarioSchema.validateAsync(dados, {abortEarly: false})
+        
+        let senha = await bcrypt.hash(dados.Senha, 10)
+        
+        db.collection("usuarios").insertOne({...dados, "Senha": senha})
+        console.log("usuario cadastrado com sucesso")
         return res.sendStatus(200)
 
     }catch(e){
